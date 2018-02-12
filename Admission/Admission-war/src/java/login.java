dcbd2d672143b212ad8c0acd995220cfd5654910
sarath kumar <sk331857@gmail.com>
2018-02-13 00:17:01 +0530

    Update login.java

--- Admission/Admission-war/src/java/login.java
+++ Admission/Admission-war/src/java/login.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import ejb.logicEJBRemote;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,10 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Abdul
- */
 @WebServlet(urlPatterns = {"/login"})
 public class login extends HttpServlet {
     @EJB
