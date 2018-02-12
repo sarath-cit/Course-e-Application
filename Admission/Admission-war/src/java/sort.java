670a667ee3ba885a8cb49be3bd1864793ca6ee88
sarath kumar <sk331857@gmail.com>
2018-02-13 00:18:21 +0530

    Update sort.java

--- Admission/Admission-war/src/java/sort.java
+++ Admission/Admission-war/src/java/sort.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -12,10 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Abdul
- */
 @WebServlet(urlPatterns = {"/sort"})
 public class sort extends HttpServlet {
 
