c077ffb120a8301f1674c18345c2cac4565e5d20
sarath kumar <sk331857@gmail.com>
2018-02-13 00:13:51 +0530

    Update logicEJB.java

--- Admission/Admission-ejb/src/java/ejb/logicEJB.java
+++ Admission/Admission-ejb/src/java/ejb/logicEJB.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejb;
 
 import javax.ejb.Stateless;
 import java.sql.*;
 
-/**
- *
- * @author Abdul
- */
 @Stateless
 public class logicEJB implements logicEJBRemote {
     private boolean flag=false;
