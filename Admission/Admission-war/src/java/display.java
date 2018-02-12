72e33b3350b611936138131ad9c31ce1ad9fd82e
sarath kumar <sk331857@gmail.com>
2018-02-13 00:16:07 +0530

    Update display.java

--- Admission/Admission-war/src/java/display.java
+++ Admission/Admission-war/src/java/display.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -13,10 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 
-/**
- *
- * @author Abdul
- */
 @WebServlet(urlPatterns = {"/display"})
 public class display extends HttpServlet {
 
